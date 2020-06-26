9e1bd996fe0d35b9fc37217ab9ce0f85ffe4fffa
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-06-25 23:06:56 -0500

    Definitely not testing in production

--- api/callback.js
+++ api/callback.js
@@ -1,6 +1,46 @@
 // write the node version of callback.test.js here lol
 
+const axios = require('axios')
+
 module.exports = (req, res) => {
-    console.log(req)
-    console.log(res)
+
+    const url = "https://github.com/login/oauth/access_token"
+    const CLIENT_ID = process.env.GH_CLIENT_ID
+    const CLIENT_SECRET = process.env.GH_CLIENT_SECRET
+    axios.defaults.headers.post['Content-Type'] = 'application/json';
+    // // this has the url params
+    // console.log(req.query)
+    const code = req.query["code"]
+    if (code != undefined) {
+        // means github has given us the code to generate an access token with
+        axios.post(url, {
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                code: code,
+            })
+            .then(function(response) {
+                token = response["access_token"]
+                res.redirect(200, `${req.host}/?token=${token}`);
+            })
+            .catch(function(error) {
+                res.redirect(200, `${req.host}/?error=${encodeURIComponent(error)}`);
+            });
+    } else if (length(req.query) > 0) {
+        // probably means github gave an error
+        res.redirect(200, `${req.host}/?error=${encodeURIComponent(req.query)}`);
+    } else {
+        // if we get here, we didn't have any other random URL params, or the code that we need to generate the access token, so we need to get github to give it to us.
+        res.redirect(200, `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=gist&redirect_uri=${encodeURIComponent(req.host)}`);
+    }
+
+    // // this has any data that was POSTed
+    // console.log(req.body)
+
+    // // this lets you redirect users
+    // res.redirect(200, '/');
+
+    // console.log("\n\n\nye ye\n\n\n")
+    // console.log(res)
+
+    //res.status(200).send()
 }
